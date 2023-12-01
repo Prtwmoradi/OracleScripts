7b32d0338ecb5f72dfbe42df84682a7a3247ce29
Partow <148716167+Prtwmoradi@users.noreply.github.com>
2023-12-01 10:55:01 +0100

    Add files via upload
    Preventing user from creating a table without schema

---
+++ table trigger.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE TRIGGER prevent_table_creation
+BEFORE CREATE ON SCHEMA
+DECLARE
+   v_object_type   VARCHAR2 (30);
+   v_object_name   VARCHAR2 (30);
+BEGIN
+   IF ora_dict_obj_type = 'TABLE' THEN
+      -- Extract schema, object type, and object name from the statement
+      v_object_type := ora_dict_obj_type;
+      v_object_name := ora_dict_obj_name;
+
+      -- Check if schema is specified
+      IF INSTR (v_object_name, '.') = 0 THEN
+         RAISE_APPLICATION_ERROR (
+            -20001,
+            'Table creation without schema is not allowed.'
+         );
+      END IF;
+   END IF;
+END prevent_table_creation;
+/
